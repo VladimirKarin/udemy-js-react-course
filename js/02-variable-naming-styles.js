65e69cf31f9f79dfd9f69b062184b75604d9f772
Vladimir Karin <Vladimir.Karin@gmail.com>
2024-04-16 22:31:13 +0300

    Lesson: variable naming styles.

---
+++ js/02-variable-naming-styles.js
@@ -0,0 +1,41 @@
+/*
+const a = 5000;
+const b = 4000;
+
+console.log(`The width of the car is ${a}, and the length is ${b}`);
+*/
+
+const carWidth = 5000;
+const carLength = 4000;
+
+console.log(
+    `The width of the car is ${carWidth}, and the length is ${carLength}`
+);
+
+// ['Stepaaan', 'Jhonny', 'Cap', 'The noisy one'].map(
+// employee =>
+// );
+
+//Server request
+
+//CamelCase
+//Best way to name variables in JS.
+const camelCase = 0;
+
+//snake_case
+//Best way of naming folders and files, but NOT variables.
+const snake_case = 0;
+
+//UPPER_SNAKE_CASE
+//best way creating JS constant variables, that are not going to be changed.
+const UPPER_SNAKE_CASE = 'http://some.url';
+
+//Same as previouse. Variable that shouldn't be changed.
+const _apiBase = 'http//some.api.url';
+const _apiKey = 'Some Randome key';
+
+//Kebab-case
+//Used for naming folders and files.
+
+//PascalCase
+//Used for naming Classes in Js.
