4c24d4cd34be6d79045df7d7fdf5f501102f208b
Vladimir Karin <Vladimir.Karin@gmail.com>
2024-04-21 14:45:44 +0300

    file update

---
+++ js/08-interpolation-concatenation.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// INTERPOLATION
+// interpolation is the process of inserting strings or values into an existing string for various purposes.
+
+const category = 'toys';
+
+console.log(`http://someawesomeurl.com/${category}`);
+
+// CONCATENATION
+// The same + operator you use for adding two numbers can be used to concatenate two strings.
+
+console.log('http://someawesomeurl.com/' + category);
+
+const user = 'Vladimir';
+alert(`Hello, ${user}!`);
