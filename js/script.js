9a1b5c5fc32ddf72c2216d19661f88038f4a5ddb
Vladimir Karin <Vladimir.Karin@gmail.com>
2024-06-20 22:53:45 +0300

    Lesson about object prototype creation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+let str = 'string';
+let strObj = new String(str);
+
+console.log(str);
+console.log(strObj);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    attack: 50,
+    sayHello: function () {
+        console.log('Yes, Sir!');
+    },
+};
+
+const johnConner = {
+    health: 100,
+};
+
+//Depricated version of creating a prototype
+johnConner.__proto__ = soldier; // Old version of prototype usage. Not being used. Depricated.
+
+console.log(johnConner);
+console.log(johnConner.armor);
+johnConner.sayHello();
+
+//Usable version of creating a prototype
+
+Object.setPrototypeOf(johnConner, soldier);
